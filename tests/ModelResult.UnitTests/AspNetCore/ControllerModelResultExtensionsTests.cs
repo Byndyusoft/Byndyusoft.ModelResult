933e5e74975dd9e68279834cb6df60a4d7c7a0fd
victor.govorov <victorgovorov@mail.ru>
2020-11-13 18:42:02 +0500

    переделал структуру проекта

---
+++ tests/ModelResult.UnitTests/AspNetCore/ControllerModelResultExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using Byndyusoft.ModelResult.Extensions;
+using Byndyusoft.ModelResult.ModelResults;
+using NUnit.Framework;
+
+namespace ModelResult.UnitTests.AspNetCore
+{
+    public class ControllerModelResultExtensionsTests
+    {
+        [Test]
+        public void ToActionResult_Generic_OkModel()
+        {
+            var modelResult = new OkModelResult<long>(1);
+            var actionResult = modelResult.ToActionResult();
+            Assert.That(actionResult.Value, Is.EqualTo(modelResult.Result));
+        }
+    }
+}
